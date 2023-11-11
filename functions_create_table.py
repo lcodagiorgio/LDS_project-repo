ca6dfc8e340c53dbdc64217896f613fb65b876f8
Bernardo D'Agostino <b.dagostino2@studenti.unipi.it>
2023-11-11 18:05:53 -0500

    commented and renamed files

--- functions_create_table.py
+++ functions_create_table.py
@@ -6,6 +6,15 @@ import requests
 
 
 def check_if_in_list(l_item, l_list):
+    """Function to check if an item is in a list.
+
+    Args:
+        l_item (list): list containing the values of a row
+        l_list (list of lists): list of lists containing the values of the rows already added to the new table 
+
+    Returns:
+        int: the index of the row in the new table if the item is in the list, -1 otherwise
+    """
     i = 0
     for i in range(len(l_list)):
         if l_item == l_list[i][0:len(l_item)]:
@@ -15,6 +24,15 @@ def check_if_in_list(l_item, l_list):
 
 
 def find_ind(col_names, col_of_int):
+    """Function to find the indexes of the columns of interest.
+
+    Args:
+        col_names (list): list of the column names of the original table
+        col_of_int (list): list of the column names of interest
+
+    Returns:
+        ind (list): list of the indexes of the columns of interest in the original table
+    """
     ind = []
     for i in range(len(col_names)):
         if col_names[i] in col_of_int:
@@ -23,12 +41,31 @@ def find_ind(col_names, col_of_int):
 
 
 def write_to_csv(file, path):
+    """Function to write a list of lists to a csv file.
+
+    Args:
+        file (list of list): list of lists containing the values of the rows of the new table
+        path (string): path of the csv file to write
+    """
     with open(path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(file)
 
 
 def add_crime_gravity(data, age_path="DATA//dict_participant_age.json", status_path="DATA//dict_participant_status.json", type_path="DATA//dict_participant_type.json"):
+    """Function to add the crime gravity attribute to the original table.
+
+    Args:
+        data (list of lists): list of lists containing the values of the rows of the original table
+        age_path (str, optional): Path of the json related to p_age. Defaults to "DATA//dict_participant_age.json".
+        status_path (str, optional): Path of the json related to p_status. Defaults to "DATA//dict_participant_status.json".
+        type_path (str, optional): Path of the json related to p_type. Defaults to "DATA//dict_participant_type.json".
+
+    Returns:
+        data (list of lists): list of lists containing the values of the rows of the new table
+    """
+    
+    
     # load json file content as a dictionary
     with open(age_path) as f:
         dic_p_age = json.load(f)
@@ -39,85 +76,78 @@ def add_crime_gravity(data, age_path="DATA//dict_participant_age.json", status_p
     with open(type_path) as f:
         dic_p_type = json.load(f)
 
+    # define the columns of interest and find their indexes
     col_of_int = ['participant_age_group', 'participant_status', 'participant_type']
     col_id = find_ind(data[0], col_of_int)
 
-    crime_gravity = []
-
+    # add the crime gravity column to the header row
     data[0].append('crime_gravity')
 
-    # for each row of the dataset minus the header
+    # for each row of the dataset minus the header row calculate the crime gravity index
     for obs in data[1:]:
         obs.append(dic_p_age[obs[col_id[0]]] * dic_p_status[obs[col_id[1]]] * dic_p_type[obs[col_id[2]]])
 
     return data
 
 
-
-def add_unique_ids(data, colums, id_name='id', path = 'DATA//NEW_TAB//NEW_TAB.csv'):
-    new_table = []
-    id_count = 0
-
-    col_in = find_ind(data[0], colums)
-
-    data[0].append(id_name)
-    for i in sorted(col_in, reverse=True):
-        data[0].pop(i)
-        
-    new_table.append(colums + [id_name])
-
-    for row in data[1:]:
-        new_row = [row[i] for i in col_in]
-        ind = check_if_in_list(new_row, new_table)
-
-        if ind == -1:
-            id_count += 1
-            new_row.append(id_count)
-            new_table.append(new_row)
-        else:
-            id = new_table[ind][-1]
-            new_row.append(id)
-
-        row.append(new_row[-1])
-
-        for i in sorted(col_in, reverse=True):
-            row.pop(i)
-
-    
-    write_to_csv(new_table, path)
-
-
-    return data, new_table
-
-
 def add_unique_ids_2(data, columns, id_name='id', path = 'DATA//NEW_TAB//NEW_TAB.csv'):
+    """Function that subdivides the original table into two new tables keeping lossless join
+
+    Args:
+        data (list of lists): list of lists containing the values of the rows of the original table
+        columns (list): list of the columns of interest to create the new tables with 
+        id_name (str, optional): the name of the newly created id column. Defaults to 'id'.
+        path (str, optional): path where to create the new csv file. Defaults to 'DATA//NEW_TAB//NEW_TAB.csv'.
+
+    Returns:
+        data (list of lists): the old table without the columns of interest
+        new_table (list of lists): the new table with the columns of interest and the new id column
+    """
+    
+    # create the new table
     new_table = []
+    # initialize the id counter
     id_count = 0
 
+    # find the indexes of the columns of interest
     col_in = find_ind(data[0], columns)
 
+    # add the new id column to the header row
     data[0].append(id_name)
+
+    # remove the columns of interest from the header row
     for i in sorted(col_in, reverse=True):
         data[0].pop(i)
-        
+    
+    # add the columns of interest and the new id column to the new table
     new_table.append(columns + [id_name])
+
+    # initialize the dictionary that will contain the unique rows of the 
+    # columns of interest as keys and the id as values
     id_dict = {}
 
+    # for each row of the dataset minus the header row
     for row in data[1:]:
+        # create a tuple with the values of the columns of interest
         new_row = tuple(row[i] for i in col_in)
 
+        # if the tuple is not in the dictionary add it as a key and the id as value
         if new_row not in id_dict:
             id_dict[new_row] = id_count
             new_table.append(list(new_row) + [id_count])
             id_count += 1
         
+        # once the tuple is in the dictionary add the id as value to the row
         row.append(id_dict[new_row])
 
+        # remove the columns of interest from the row
         for i in sorted(col_in, reverse=True):
             row.pop(i)
 
+    # write the new table to a csv file
     write_to_csv(new_table, path)
     
+    # return the old table without the columns of interest and the new table
     return data, new_table
 
 
@@ -242,3 +272,43 @@ def add_geo_attributes(oldGeo_csv, newGeo_csv):
 
                 newrow = {"geo_id":geo_id, "latitude":latitude, "longitude":longitude, "city":city, "state":state, "country":country}
                 dwriter.writerow(newrow)
+
+
+    
+def add_unique_ids(data, colums, id_name='id', path = 'DATA//NEW_TAB//NEW_TAB.csv'):
+    # first version of the function 
+    new_table = []
+    id_count = 0
+
+    col_in = find_ind(data[0], colums)
+
+    data[0].append(id_name)
+    for i in sorted(col_in, reverse=True):
+        data[0].pop(i)
+        
+    new_table.append(colums + [id_name])
+
+    for row in data[1:]:
+        new_row = [row[i] for i in col_in]
+        ind = check_if_in_list(new_row, new_table)
+
+        if ind == -1:
+            id_count += 1
+            new_row.append(id_count)
+            new_table.append(new_row)
+        else:
+            id = new_table[ind][-1]
+            new_row.append(id)
+
+        row.append(new_row[-1])
+
+        for i in sorted(col_in, reverse=True):
+            row.pop(i)
+
+    
+    write_to_csv(new_table, path)
+
+
+    return data, new_table
+
+
--- functions_upload_tables.py
+++ functions_upload_tables.py
@@ -2,30 +2,59 @@ import pyodbc
 import csv
 from functions_create_table import *
 def connect_to_server(server, database, username, password, driver):
+    """Function to connect to the server
+
+    Args:
+        server (str): Name of the server
+        database (str): Name of the database
+        username (str): Username
+        password (str): Password
+        driver (str): Driver
+
+    Returns:
+        conn (Connection obj): Connection object from pyodbc module
+    """
+
+    # 
     connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    
     # Connect to the database
     try:
         conn = pyodbc.connect(connection_string)
         print("Connection successful")
     except pyodbc.Error as e:
         print(f"Error connecting to database: {e}")
+
+
     return conn
 
 def get_tables_col_types(conn):
+    """Function to get the column names and their types of all tables in the database
+
+    Args:
+        conn (Connection obj): Connection object from pyodbc module
+
+    Returns:
+        table_data (dict): Dictionary with table names as keys and two lists one of column names and one of column types as values
+    """
+    # Create a cursor
     cursor = conn.cursor()
 
-    # Get all table names
+    # Get all table names with a query
     cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE'")
     tables = [table[0] for table in cursor.fetchall()]
 
     # Initialize an empty dictionary to store the table data
     table_data = {}
+
+    # initialize a counter to skip the first table
     i = 0
     # For each table, get column names and types
     for table in tables:
         i = i + 1
         if i == 1:
             continue
+        # Get column names and types with a query
         cursor.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'")
         columns = cursor.fetchall()
 
@@ -41,41 +70,78 @@ def get_tables_col_types(conn):
         # Add the lists to the dictionary
         table_data[table] = [column_names, column_types]
 
+    # Close the cursor
     cursor.close()
+
+
     return table_data
 
 
 def reorder_values(col_names1, col_names2, values):
+    """Function to reorder values according to a list of column names
+
+    Args:
+        col_names1 (list): list of column names in incorrect order
+        col_names2 (list): list of column names in correct order
+        values (list): list of values in incorrect order
+
+    Returns:
+        reordere_values (list): list of values in correct order 
+    """
     # create a dictionary with the first column names as keys and the values as values
     dict_values = dict(zip(col_names1, values))
     # create a list with the values ordered as the second column names
     reordered_values = [dict_values[col_name] for col_name in col_names2]
+
+
     return reordered_values
 
 
 def upload_table(table_name, conn, tab_data):
+    """Function to upload a table to the database
+
+    Args:
+        table_name (str): Name of the table
+        conn (Connection obj): Connection object from pyodbc module
+        tab_data (list of lists): list of lists with the column names as the first elemente and types as second element
+    """
+    # Create a cursor
     cursor = conn.cursor()
 
+    # Open the csv file
     with open(f"new_tables/{table_name}.csv", newline='', encoding='utf-8') as csvfile:
+        # Read the csv file
         reader = csv.reader(csvfile)
+        # Get the column names of the csv file
         columns_csv = next(reader)
+        # Get the column names of the database table
         columns_db = tab_data[0]
+        # Create a string with the column names of the database table
         columns_s = ','.join(columns_db)
 
+        # Initialize a list to store the values strings
         values_list = []
+
+        # Iterate over the rows of the csv file
         for row in reader:
+            # Reorder the values according to the database table column names
             values = reorder_values(columns_csv, columns_db, row)
+            # If the datatype is a string, formati it correctly for the SQL query
             for i in range(len(tab_data[1])):
                 if tab_data[1][i] in ['nvarchar', 'date', 'char', 'varchar']:
                     values[i] = values[i].replace("'", "''")
                     values[i] = f"'{values[i]}'"
+            # Add the values to the list as a unique string
             values_list.append('(' + ','.join(values) + ')')
 
+            # If the list has 1000 values, create a query and execute it
             if len(values_list) == 1000:
                 values_str = ','.join(values_list)
                 query = f"INSERT INTO {table_name}({columns_s}) VALUES {values_str}"
-                print(query)
+                #print(query)
+                # Execute the query
                 cursor.execute(query)
+                # Reset the list
                 values_list = []
 
         # Insert remaining values if they are less than 100
@@ -84,5 +150,6 @@ def upload_table(table_name, conn, tab_data):
             query = f"INSERT INTO {table_name}({columns_s}) VALUES {values_str}"
             #print(query)
             cursor.execute(query)
-
+            
+    # Close the cursor
     cursor.close()
