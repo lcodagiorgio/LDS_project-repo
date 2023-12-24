b62f077e958b135139d95ad56c018310299de62c
Bernardo D'Agostino <b.dagostino2@studenti.unipi.it>
2023-12-24 17:45:02 +0100

    zip codes adde

--- upload_geograpy_corrected.py
+++ upload_geograpy_corrected.py
@@ -1,11 +1,26 @@
 from functions_upload_tables import *
 import csv
 path = 'new_tables\\Geography_corr.csv'
+path_dat = 'DATA\\uscities.csv'
 
 # open path and read csv with correct data
 with open(path, 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
+
+# open path_dat and read csv with correct data to add zip code
+with open(path_dat, 'r') as f:
+    reader = csv.reader(f)
+    data_dat = list(reader)
+
+# find index for city, state, zips
+for i, name in enumerate(data_dat[0]):
+    if name == 'city':
+        city_index = i
+    elif name == 'state_name':
+        state_index = i
+    elif name == 'zips':
+        zips_index = i
 
 # Set up the connection string
 server = 'tcp:lds.di.unipi.it'
@@ -30,6 +45,19 @@ for row in data:
     # extract values from the row
     geo_id, latitude, longitude, city, state, country = row
 
+    # given the city and state find the postal code in data_dat
+    for row_dat in data_dat:
+        city_dat = row_dat[city_index]
+        state_dat = row_dat[state_index]
+        zips_dat = row_dat[zips_index]
+        if city_dat == city and state_dat == state:
+            break
+
+    # convert zips_dat to string and take first charaters until the first space to only select first zip code
+    zips_dat = str(zips_dat)
+    zip_code = zips_dat[:zips_dat.find(' ')]
+
+
     # Replace single quotes in the string values with two single quotes to escape them for SQL queries
     city = city.replace("'", "''")
     state = state.replace("'", "''")
@@ -38,7 +66,7 @@ for row in data:
     # Create the query
     query = f"""
     UPDATE Geography
-    SET latitude = {latitude}, longitude = {longitude}, city = '{city}', state = '{state}', country = '{country}'
+    SET latitude = {latitude}, longitude = {longitude}, city = '{city}', state = '{state}', country = '{country}', zip_code = '{zip_code}'
     WHERE geo_id = {geo_id}
     """
 
